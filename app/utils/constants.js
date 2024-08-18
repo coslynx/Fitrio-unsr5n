77d85a004da0bd6971eccda343710093ce25c8da
Spectra.codes by Drix10 <drixisgod@gmail.com>
2024-08-18 17:13:36 +0530

    generated file: `app/utils/constants.js`

---
+++ app/utils/constants.js
@@ -0,0 +1,13 @@
+export const GoalTypes = [
+  "Weight Loss",
+  "Muscle Gain",
+  "Distance Running",
+  "Custom",
+];
+
+export const ActivityTypes = [
+  "Cardio",
+  "Strength Training",
+  "Yoga",
+  "Other",
+];
